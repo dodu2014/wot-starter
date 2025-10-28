f7d22f5485b6733f49609b318191008dcf8a040a
tnt group <dodu@live.cn>
2025-10-29 03:08:33 +0800

    feat: 尝试扩展 'uni-mini-router'

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,6 +1,30 @@
+// 扩展uni-mini-router的RoutePathLocation接口
+import type { RouteNameLocation } from 'uni-mini-router'
+
 export {}
 
 declare module 'vue' {
   type Hooks = App.AppInstance & Page.PageInstance
   interface ComponentCustomOptions extends Hooks {}
+}
+
+// 'uni-mini-router' 模块的类型定义
+
+// 扩展RoutePathLocation接口，使path支持自定义类型
+declare module 'uni-mini-router' {
+  export interface RoutePathLocation {
+    path: _LocationUrl
+  }
+
+  /** 原始本地路由 */
+  export type RouteLocationRaw = _LocationUrl | RouteNameLocation | RoutePathLocation
+
+  // 扩展 Router 接口的方法签名
+  export interface Router {
+    push: (to: RouteLocationRaw) => void
+    replace: (to: RouteLocationRaw) => void
+    replaceAll: (to: RouteLocationRaw) => void
+    pushTab: (to: RouteLocationRaw) => void
+    needLogin?: boolean
+  }
 }

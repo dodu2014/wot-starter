1463a28a93c886343fc4669f378571ff62f3f2c1
tnt group <dodu@live.cn>
2025-11-10 21:49:32 +0800

    feat: 重新封装缺省提示为 wd-empty 组件
    组件使用的资源已在服务端支持

---
+++ src/components/wd-empty.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'WdEmpty',
+  inheritAttrs: true,
+})
+const baseUrl = import.meta.env.VITE_API_BASE_URL
+</script>
+
+<template>
+  <wd-status-tip :url-prefix="`${baseUrl}/images/default/`" />
+</template>
+
+<style lang="scss" scoped>
+:deep() {
+  .wd-status-tip__image {
+    opacity: 0.15;
+  }
+}
+</style>

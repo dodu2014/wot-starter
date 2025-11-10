17189ee4c377edd924d21c54d53c135052e59c26
tnt group <dodu@live.cn>
2025-11-10 22:28:36 +0800

    feat: 让页面布局使用 flex-col 布局

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ onLaunch(() => {})
   min-height: calc(100vh - var(--window-top));
   box-sizing: border-box;
   background: #f9f9f9;
+
+  display: flex;
+  flex-direction: column;
 }
 
 .wot-theme-dark.page-wraper {

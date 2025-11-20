1ced6f33e142d66c8aa59dd74aa29c85eba4e636
tnt group <dodu@live.cn>
2025-11-21 00:28:53 +0800

    fix: 修复无法在 App.onLoad 中正常调用版本检测的问题

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,8 +1,5 @@
 import pack from '../../package.json'
 
-const { alert } = useGlobalMessage()
-const { loading, close: hideLoading } = useGlobalLoading()
-
 /** 获取小程序版本信息 */
 export function getMiniProgramVersion() {
   // #ifdef MP
@@ -33,44 +30,47 @@ export function getMiniProgramVersion() {
  * @returns 版本更新管理器对象
  */
 export function checkMiniProgramUpdate(notUpdatePrompt = '') {
-  // 小程序更新
+  // 全局消息提示（必须在方法局部定义，全局定义会报错）
+  const { alert } = useGlobalMessage()
+  const { loading, close: hideLoading } = useGlobalLoading()
+
+  // 返回全局唯一的版本更新管理器对象： updateManager，用于管理小程序更新
   const updateManager = uni.getUpdateManager()
   // 分享到朋友圈时，updateManager 为 null
   if (!updateManager)
     return
 
-  // 检测更新
+  // 当向应用后台请求完新版本信息，会进行回调
   updateManager.onCheckForUpdate((res) => {
     // 请求完新版本信息的回调
-    console.log('检测小程序更新...', res.hasUpdate)
     if (res.hasUpdate)
       loading('版本更新中..')
     else if (notUpdatePrompt)
       alert(notUpdatePrompt)
   })
 
-  // 应用更新
-  updateManager.onUpdateReady(async (res: any) => {
-    console.log('onUpdateReady', res)
+  // 当新版本下载完成，会进行回调
+  updateManager.onUpdateReady(async (res: { version?: string }) => {
     hideLoading()
     alert({
       title: '版本更新',
-      msg: '新版本已经准备好，是否更新并重启应用？',
+      msg: `新版本 v${res.version || ''} 已经准备好，是否更新并重启应用？`,
       closeOnClickModal: false,
       beforeConfirm: ({ resolve }) => {
+        // 当新版本下载完成，调用该方法会强制当前uni-app应用上新版本并重启
         updateManager.applyUpdate()
         resolve(true)
       },
     })
   })
 
-  updateManager.onUpdateFailed((res) => {
+  // 当新版本下载失败，会进行回调
+  updateManager.onUpdateFailed((res: { version?: string }) => {
     // 新的版本下载失败
-    console.log('onUpdateFailed', res)
     hideLoading()
     alert({
       title: '版本更新',
-      msg: '新版本下载失败，请检查网络！',
+      msg: `新版本 v${res.version || ''} 下载失败，请检查网络！`,
       closeOnClickModal: false,
     })
   })

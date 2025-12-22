267e6940c90e7d947d10ad9e8cd5fe9ff45f0448
tnt group <dodu@live.cn>
2025-12-22 14:09:13 +0800

    feat: 增加封装的 CitysPicker 组件

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -183,6 +183,7 @@ declare global {
   const useClipboard: typeof import('@vueuse/core')['useClipboard']
   const useClipboardItems: typeof import('@vueuse/core')['useClipboardItems']
   const useCloned: typeof import('@vueuse/core')['useCloned']
+  const useColPickerData: typeof import('./composables/useColPickerData')['useColPickerData']
   const useColorMode: typeof import('@vueuse/core')['useColorMode']
   const useConfirmDialog: typeof import('@vueuse/core')['useConfirmDialog']
   const useCounter: typeof import('@vueuse/core')['useCounter']
@@ -535,6 +536,7 @@ declare module 'vue' {
     readonly useClipboard: UnwrapRef<typeof import('@vueuse/core')['useClipboard']>
     readonly useClipboardItems: UnwrapRef<typeof import('@vueuse/core')['useClipboardItems']>
     readonly useCloned: UnwrapRef<typeof import('@vueuse/core')['useCloned']>
+    readonly useColPickerData: UnwrapRef<typeof import('./composables/useColPickerData')['useColPickerData']>
     readonly useColorMode: UnwrapRef<typeof import('@vueuse/core')['useColorMode']>
     readonly useConfirmDialog: UnwrapRef<typeof import('@vueuse/core')['useConfirmDialog']>
     readonly useCounter: UnwrapRef<typeof import('@vueuse/core')['useCounter']>
---
+++ src/components/CitysPicker.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import type { ColPickerColumnChangeOption } from 'wot-design-uni/components/wd-col-picker/types'
+
+defineOptions({ name: 'CitysPicker' })
+
+// eslint-disable-next-line unused-imports/no-unused-vars
+const props = withDefaults(defineProps<Props>(), {
+  label: '行政区域',
+  labelWidth: '75px',
+  required: false,
+  markerSide: 'after',
+})
+
+const model = defineModel<string[]>('value', { default: () => [] })
+const modelLabels = defineModel<string[]>('labels', { default: () => [] })
+
+interface Props {
+  label?: string
+  labelWidth?: string
+  required?: boolean
+  markerSide?: 'before' | 'after'
+  prop?: string
+}
+const { colPickerData, findChildrenByCode } = useColPickerData()
+const columns = ref<any[]>([
+  colPickerData.map(item => ({ value: item.value, label: item.text })),
+])
+function columnChange({ selectedItem, resolve, finish }: ColPickerColumnChangeOption) {
+  console.log(selectedItem, resolve, finish)
+  const areaData = findChildrenByCode(colPickerData, selectedItem.value)
+  if (areaData && areaData.length) {
+    resolve(areaData.map(item => ({ value: item.value, label: item.text })))
+  }
+  else {
+    finish()
+  }
+}
+
+/**
+ * 最后一列选项选中时触发
+ *
+ * @param e - 事件对象
+ * @param e.value - 选项值数组
+ * @param e.selectedItems - 选项数组 ({ value, label })
+ */
+function handleConfirm(e: { value: string[], selectedItems: { value: string, label: string }[] }): void {
+  const { selectedItems } = e
+  modelLabels.value = selectedItems.map(item => item.label)
+}
+</script>
+
+<template>
+  <wd-col-picker
+    v-model="model"
+    :label="label"
+    :label-width="labelWidth"
+    :prop="prop"
+    :required="required"
+    :marker-side="markerSide"
+    :columns="columns"
+    :column-change="columnChange"
+    auto-complete
+    @confirm="handleConfirm"
+  />
+</template>

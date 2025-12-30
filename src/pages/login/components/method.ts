817540b8cae6e06a68fd3509b3fd58f7ee388ce4
tnt group <dodu@live.cn>
2025-12-31 00:04:51 +0800

    feat: 登录同意服务协议确认框关闭 modal 点击关闭

--- src/pages/login/components/method.ts
+++ src/pages/login/components/method.ts
@@ -11,6 +11,7 @@ export function checkAccept(agreed: boolean): Promise<boolean> {
       title: '同意并继续',
       msg: '请确认以及阅读且同意‘用户服务协议’和‘隐私政策’',
       confirmButtonText: '已阅读',
+      closeOnClickModal: false,
       success: (e) => {
         if (e.action === 'confirm') {
           agreed = true

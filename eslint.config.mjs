21cbf1eb349c9312dcf44a7da8ae9891738429d5
tnt group <dodu@live.cn>
2026-01-17 21:04:44 +0800

    fix: eslint 配置启用 unocss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import uniHelper from '@uni-helper/eslint-config'
+import uni from '@uni-helper/eslint-config'
 
 // 配置参考 https://github.com/antfu/eslint-config?tab=readme-ov-file#customization (工厂函数还接受任意数量的自定义配置覆盖：antfu)
-export default uniHelper(
+export default uni(
   {
-    // unocss: true,
+    unocss: true,
     rules: {
       'no-console': 'off',
       'no-ignore': 'off',

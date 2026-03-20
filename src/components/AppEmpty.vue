ba2c95ba1f92c4e483064e407b897bf8270e38d1
tnt group <dodu@live.cn>
2026-03-20 16:10:24 +0800

    feat: ✨ 重新定义 AppEmpty 组件

--- src/components/AppEmpty.vue
+++ src/components/AppEmpty.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 defineOptions({
   name: 'AppEmpty',
-  inheritAttrs: true,
 })
+
+withDefaults(
+  defineProps<{
+    image?: ImageType
+    tip?: string
+  }>(),
+  {
+    image: 'network',
+  },
+)
+
+type ImageType = 'search' | 'network' | 'content' | 'collect' | 'comment' | 'halo' | 'message'
+
 const baseUrl = import.meta.env.VITE_API_BASE_URL
 </script>
 
 <template>
-  <wd-status-tip :url-prefix="`${baseUrl}/images/default/`" custom-class="!op-15" />
+  <wd-status-tip :url-prefix="`${baseUrl}/images/default/`" custom-class="!op-65" :image="image" :tip="tip" />
 </template>

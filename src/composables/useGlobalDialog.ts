338a6ca3ec3fb369dfc78ec557ed53810d05e582
tnt group <dodu@live.cn>
2026-06-16 01:01:17 +0800

    feat: ✨ 实现 confirm 的异步方式

--- src/composables/useGlobalDialog.ts
+++ src/composables/useGlobalDialog.ts
@@ -11,92 +11,92 @@ interface GlobalDialog {
   currentPage: string
 }
 
-type DialogType = NonNullable<DialogOptions['type']>
-
-function isButtonPropsObject(value: unknown): value is Record<string, any> {
-  return value !== null && CommonUtil.isObj(value)
-}
-
-function normalizeButtonProps(props: unknown, text?: string) {
-  if (props === null) {
-    return null
-  }
-
-  if (isButtonPropsObject(props)) {
-    return {
-      ...props,
-      ...(text ? { text } : {}),
-    }
-  }
-
-  if (CommonUtil.isString(props) || text) {
-    return {
-      text: text || props,
-    }
-  }
-
-  if (props === undefined) {
-    return {}
-  }
-
-  return props
-}
-
-function withDefaultTypeOptions(option: GlobalDialogOptions, type?: DialogType): GlobalDialogOptions {
-  const next: GlobalDialogOptions = {
-    ...option,
-    ...(type ? { type } : {}),
-  }
-
-  if (next.showCancelButton === undefined) {
-    if (next.type === 'alert') {
-      next.showCancelButton = false
-    }
-    else if (next.type === 'confirm' || next.type === 'prompt') {
-      next.showCancelButton = true
-    }
-  }
-
-  return next
-}
-
-function normalizeDialogOptions(option: GlobalDialogOptions, type?: DialogType): GlobalDialogOptions {
-  const next = withDefaultTypeOptions(option, type)
-
-  next.confirmButtonProps = normalizeButtonProps(next.confirmButtonProps, next.confirmButtonText) as DialogOptions['confirmButtonProps']
-
-  if (next.showCancelButton === false) {
-    next.cancelButtonProps = null
-  }
-  else if (next.showCancelButton === true || next.cancelButtonProps !== undefined || next.cancelButtonText) {
-    next.cancelButtonProps = normalizeButtonProps(next.cancelButtonProps, next.cancelButtonText) as DialogOptions['cancelButtonProps']
-  }
-
-  return next
-}
-
-function normalizeOption(option: GlobalDialogOptions | string, type?: DialogType): GlobalDialogOptions {
-  return normalizeDialogOptions(CommonUtil.isString(option) ? { title: option } : option, type)
-}
-
 export const useGlobalDialog = defineStore('global-Dialog', {
   state: (): GlobalDialog => ({
     dialogOptions: null,
     currentPage: '',
   }),
   actions: {
-    show(option: GlobalDialogOptions | string, type?: DialogType) {
+    show(option: GlobalDialogOptions | string) {
+      const opt = (CommonUtil.isString(option) ? { title: option } : option)
+      opt.cancelButtonProps = {
+        text: 'Cancel',
+        round: false,
+        variant: 'base',
+        ...opt.cancelButtonProps as any,
+      }
+      opt.confirmButtonProps = {
+        text: 'Confirm',
+        customClass: '!bg-primary-gradient',
+        round: false,
+        ...opt.confirmButtonProps as any,
+      }
       this.currentPage = getCurrentPath()
-      this.dialogOptions = normalizeOption(option, type)
+      this.dialogOptions = {
+        cancelButtonText: 'Cancel',
+        confirmButtonText: 'Confirm',
+        ...opt,
+      }
+    },
+    /**
+     * 显示异步消息对话框, options 中的 success 和 fail 回调将被忽略
+     * @param {GlobalDialogOptions | string} option - 消息选项对象或消息标题字符串
+     * @returns {Promise<DialogResult>} 返回一个 Promise，当用户点击确认按钮时 resolve，点击取消或关闭时 reject
+     * @throws {DialogResult} 当用户取消操作或对话框失败时抛出 DialogResult 对象
+     */
+    showAsync(option: GlobalDialogOptions | string): Promise<DialogResult> {
+      return new Promise<DialogResult>((resolve, reject) => {
+        const opt = (CommonUtil.isString(option) ? { title: option } : option)
+        opt.cancelButtonProps = {
+          text: 'Cancel',
+          round: false,
+          variant: 'base',
+          ...opt.cancelButtonProps as any,
+        }
+        opt.confirmButtonProps = {
+          text: 'Confirm',
+          customClass: '!bg-primary-gradient',
+          round: false,
+          ...opt.confirmButtonProps as any,
+        }
+        this.currentPage = getCurrentPath()
+        this.dialogOptions = {
+          cancelButtonText: 'Cancel',
+          confirmButtonText: 'Confirm',
+          ...opt,
+          success(res) {
+            resolve(res)
+          },
+          fail(res) {
+            reject(res)
+          },
+        }
+      })
     },
     alert(option: GlobalDialogOptions | string) {
-      this.show(option, 'alert')
+      const DialogOptions = CommonUtil.deepMerge({ type: 'alert' }, CommonUtil.isString(option) ? { title: option } : option) as DialogOptions
+      DialogOptions.showCancelButton = false
+      this.show(DialogOptions)
     },
     confirm(option: GlobalDialogOptions | string) {
-      this.show(option, 'confirm')
+      const DialogOptions = CommonUtil.deepMerge({ type: 'confirm' }, CommonUtil.isString(option) ? { title: option } : option) as DialogOptions
+      DialogOptions.showCancelButton = true
+      this.show(DialogOptions)
+    },
+    /**
+     * 异步显示确认对话框
+     * @param {GlobalDialogOptions | string} option - 消息选项对象或标题字符串
+     * @returns {Promise<DialogResult>} 返回用户操作结果的 Promise
+     */
+    async confirmAsync(option: GlobalDialogOptions | string): Promise<DialogResult> {
+      const messageOptions = CommonUtil.deepMerge({ type: 'confirm' }, CommonUtil.isString(option) ? { title: option } : option) as DialogOptions
+      messageOptions.showCancelButton = true
+      return await this.showAsync(messageOptions)
     },
     prompt(option: GlobalDialogOptions | string) {
-      this.show(option, 'prompt')
+      const DialogOptions = CommonUtil.deepMerge({ type: 'prompt' }, CommonUtil.isString(option) ? { title: option } : option) as DialogOptions
+      DialogOptions.showCancelButton = true
+      this.show(DialogOptions)
     },
     close() {
       this.dialogOptions = null
